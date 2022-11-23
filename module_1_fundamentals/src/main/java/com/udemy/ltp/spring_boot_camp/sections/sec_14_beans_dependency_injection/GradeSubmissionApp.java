b02f608690c1cc9b114cb1d797b0b435c17a65d3
Minh. Vu Tuan - CMC Global DU3.31 <vtminh1@cmcglobal.vn>
2022-11-23 18:08:56 +0700

    23/11/22 | 18:10

--- module_1_fundamentals/src/main/java/com/udemy/ltp/spring_boot_camp/sections/sec_14_beans_dependency_injection/GradeSubmissionApp.java
+++ module_1_fundamentals/src/main/java/com/udemy/ltp/spring_boot_camp/sections/sec_14_beans_dependency_injection/GradeSubmissionApp.java
@@ -3,7 +3,11 @@ package com.udemy.ltp.spring_boot_camp.sections.sec_14_beans_dependency_injectio
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.udemy.ltp.spring_boot_camp.sections.sec_14_beans_dependency_injection.controller",
+	"com.udemy.ltp.spring_boot_camp.sections.sec_14_beans_dependency_injection.service",
+	"com.udemy.ltp.spring_boot_camp.sections.sec_14_beans_dependency_injection.repository"
+})
 public class GradeSubmissionApp {
 	public static void main(String[] args) {
 		SpringApplication.run(GradeSubmissionApp.class, args);
