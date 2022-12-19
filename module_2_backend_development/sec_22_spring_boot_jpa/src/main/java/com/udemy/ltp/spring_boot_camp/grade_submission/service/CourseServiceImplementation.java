b9fc66e0db53c22636d07561e4ed4cad195b28f8
Minh. Vu Tuan - CMC Global DU3.31 <vtminh1@cmcglobal.vn>
2022-12-19 18:40:49 +0700

    19/12/22 - 18:40

--- module_2_backend_development/sec_22_spring_boot_jpa/src/main/java/com/udemy/ltp/spring_boot_camp/grade_submission/service/CourseServiceImplementation.java
+++ module_2_backend_development/sec_22_spring_boot_jpa/src/main/java/com/udemy/ltp/spring_boot_camp/grade_submission/service/CourseServiceImplementation.java
@@ -1,31 +1,37 @@
 package com.udemy.ltp.spring_boot_camp.grade_submission.service;
 
 import com.udemy.ltp.spring_boot_camp.grade_submission.entity.Course;
+import com.udemy.ltp.spring_boot_camp.grade_submission.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class CourseServiceImplementation implements CourseService {
-	@Override
-	public Course getCourse(Long id) {
-		return null;
-	}
+	private CourseRepository courseRepository;
 
 	@Override
 	public Course saveCourse(Course course) {
-		return null;
+		return courseRepository.save(course);
 	}
 
 	@Override
-	public void deleteCourse(Long id) {
+	public Course getCourse(Long id) {
+		Optional<Course> course = courseRepository.findById(id);
 
+		return course.orElse(null);
 	}
 
 	@Override
 	public List<Course> getCourses() {
-		return null;
+		return (List<Course>) courseRepository.findAll();
+	}
+
+	@Override
+	public void deleteCourse(Long id) {
+		courseRepository.deleteById(id);
 	}
 }
